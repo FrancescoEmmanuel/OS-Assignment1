1c0f7b124e30f715bcec83bce1cd3be39695ba4f
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2024-03-25 22:48:49 +0700

    edit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import random
+import time
 
 LOWER_NUM = 1
 UPPER_NUM = 10000
@@ -13,14 +14,12 @@ all_file = open("all.txt", "w")
 even_file = open("even.txt", "w")
 odd_file = open("odd.txt", "w")
 
-
 def producer():
     for _ in range(MAX_COUNT):
         num = random.randint(LOWER_NUM, UPPER_NUM)
         with mutex:
             stack.append(num)
             all_file.write(f"{num}\n")
-
 
 def consumer_even():
     count = 0
@@ -31,7 +30,6 @@ def consumer_even():
                 even_file.write(f"{num}\n")
                 count += 1
 
-
 def consumer_odd():
     count = 0
     while count < MAX_COUNT:
@@ -41,23 +39,32 @@ def consumer_odd():
                 odd_file.write(f"{num}\n")
                 count += 1
 
+def main():
+    start_time = time.time()
 
-# Create threads
-producer_thread = threading.Thread(target=producer)
-consumer_even_thread = threading.Thread(target=consumer_even)
-consumer_odd_thread = threading.Thread(target=consumer_odd)
+    # Create threads
+    producer_thread = threading.Thread(target=producer)
+    consumer_even_thread = threading.Thread(target=consumer_even)
+    consumer_odd_thread = threading.Thread(target=consumer_odd)
 
-# Start threads
-producer_thread.start()
-consumer_even_thread.start()
-consumer_odd_thread.start()
+    # Start threads
+    producer_thread.start()
+    consumer_even_thread.start()
+    consumer_odd_thread.start()
 
-# Join threads
-producer_thread.join()
-consumer_even_thread.join()
-consumer_odd_thread.join()
+    # Join threads
+    producer_thread.join()
+    consumer_even_thread.join()
+    consumer_odd_thread.join()
 
-# Close files
-all_file.close()
-even_file.close()
-odd_file.close()
+    # Close files
+    all_file.close()
+    even_file.close()
+    odd_file.close()
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution Time: {execution_time} seconds")
+
+if __name__ == "__main__":
+    main()
